35f63025f11c0f8ddfa2b581be94f52972c4095b
Eduardo Farias <edo_farias@icloud.com>
2016-09-12 23:24:23 -0300

    Initial Commit

---
+++ pruebagit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebagit
+//
+//  Created by Eduardo Farías on 12-09-16.
+//  Copyright © 2016 Eduardo Farías. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
